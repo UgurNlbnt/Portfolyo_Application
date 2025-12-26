f1d8be8957da161f68c467fe5ef538ae7e26748e
UgurNlbnt <ugur.nalbant617@gmail.com>
2025-12-26 20:52:41 +0300

    v0.5

---
+++ AkademiQPortfolio/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using AkademiQPortfolio.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQPortfolio.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly portfolyoDbContext _context;
+
+        public DashboardController(portfolyoDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.firstProjest = _context.ProjectsTables.FirstOrDefault().ProjectName;
+            ViewBag.lastProjest = _context.ProjectsTables.OrderByDescending(p => p.ProjectId).FirstOrDefault().ProjectName;
+            ViewBag.totalProjects = _context.Services.Count();
+            ViewBag.FirstCategory = _context.CategoriesTables.FirstOrDefault().CategoryName;
+
+            //en çok projeye sahip kategori
+
+            //yetenekler de en büyük orana sahip yetenek
+            return View();
+        }
+    }
+}
