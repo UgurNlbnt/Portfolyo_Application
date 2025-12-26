db20ad85551d15de3a9eb1d2692b4e17354d3632
UgurNlbnt <ugur.nalbant617@gmail.com>
2025-12-26 19:57:23 +0300

    v0.5

---
+++ AkademiQPortfolio/Controllers/ProjectController.cs
@@ -0,0 +1,51 @@
+﻿using AkademiQPortfolio.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AkademiQPortfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly portfolyoDbContext _context;
+        public ProjectController(portfolyoDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _context.ProjectsTables.Include(x => x.Category).ToList();
+            return View(values);
+        }
+
+        //dropdown ---> 2 sütunu var value ve gözüken kısım
+        // kullanıcı kategory adını görecek ama value kısmında kategory id olacak
+
+        //selctListItem 
+        [HttpGet]
+        public IActionResult ProjectCreate()
+        {
+
+            //veritabanından kategorileri çekip dropdowna dolduracağız
+            ViewBag.Categories = _context.CategoriesTables.Select(x=> new SelectListItem
+            {
+                Text = x.CategoryName,
+                Value = x.CategoryId.ToString()
+            });
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProjectCreate(ProjectsTable projectsTable)
+        {
+            _context.ProjectsTables.Add(projectsTable);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //guncelelemeyı yaz 
+
+    }
+}
--- AkademiQPortfolio/ViewComponents/ExperinceViewComponent.cs
+++ AkademiQPortfolio/ViewComponents/ExperinceViewComponent.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AkademiQPortfolio.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AkademiQPortfolio.ViewComponents;
 
 public class ExperinceViewComponent : ViewComponent
 {
+    private readonly portfolyoDbContext _context;
+    public ExperinceViewComponent(portfolyoDbContext context)
+    {
+        _context = context;
+    }
     public IViewComponentResult Invoke()
     {
+        var value = _context.Services.ToList();
         return View();
     }
 }
